7550e88062920d17d5dbf22be952c95b4543a044
dqwerter <danielqin7@outlook.com>
2019-06-18 11:19:20 +0800

    rewrite routes & views

---
+++ routes/manage-user.js
@@ -0,0 +1,16 @@
+module.exports = {
+    manageUserPage: (req, res) => {
+        let query = "SELECT * FROM users ORDER BY id ASC"; // query database to get all the users
+
+        // execute query
+        db.query(query, (err, result) => {
+            if (err) {
+                res.redirect('/');
+            }
+            res.render('manage-user.ejs', {
+                title: "View users"
+                ,users: result
+            });
+        });
+    },
+};
